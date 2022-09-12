f75398c2c10c9a072b5764fbdb60b448cd024879
claurennt <c.benedetti1989@gmail.com>
2022-09-12 23:29:08 +0200

    fix register, improve authcontext, add logout and login

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,71 +1,85 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-const SignUp = ({
-  isAuthenticated,
-  handleChange,
-  handleRegister,
-  user: { username, password, email },
-}) => {
-  console.log(isAuthenticated);
-  isAuthenticated && <Navigate to='/secret' replace />;
-  return (
-    <div className='bg-grey-lighter min-h-screen flex flex-col'>
-      <div className='container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2'>
-        <div className='bg-white px-6 py-8 rounded shadow-md text-black w-full'>
-          <h1 className='mb-8 text-3xl text-center'>Sign up</h1>
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { register, saveToLocalStorage } from "../utils/authUtils";
+import { useAuthContext } from "../context/AuthContext";
 
-          <div className='mx-auto w-64 text-center '>
-            <div className='relative w-64'>
-              <div className='w-64 h-64 group bg-blue-100 hover:bg-blue-200 opacity-60 rounded-full flex justify-center items-center cursor-pointer transition duration-500'>
+const SignUp = () => {
+  const navigate = useNavigate();
+
+  const { user, setUser, setAuthToken } = useAuthContext();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const { headers } = await register(user);
+
+    setAuthToken(headers["x-authorization-token"]);
+    saveToLocalStorage(headers["x-authorization-token"]);
+    navigate("/secret", { replace: true });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  return (
+    <div className="bg-grey-lighter min-h-screen flex flex-col">
+      <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+        <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+          <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+
+          <div className="mx-auto w-64 text-center ">
+            <div className="relative w-64">
+              <div className="w-64 h-64 group bg-blue-100 hover:bg-blue-200 opacity-60 rounded-full flex justify-center items-center cursor-pointer transition duration-500">
                 <img
-                  className='w-12'
-                  src='https://www.svgrepo.com/show/33565/upload.svg'
-                  alt=''
+                  className="w-12"
+                  src="https://www.svgrepo.com/show/33565/upload.svg"
+                  alt=""
                 />
               </div>
             </div>
           </div>
           <input
-            type='text'
-            className='block border border-grey-light w-full p-3 rounded mb-4'
-            name='username'
-            placeholder='Username'
-            value={username}
+            type="text"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="username"
+            placeholder="Username"
+            value={user.username}
             onChange={handleChange}
           />
 
           <input
-            type='text'
-            className='block border border-grey-light w-full p-3 rounded mb-4'
-            name='email'
-            placeholder='Email'
-            value={email}
+            type="text"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="email"
+            placeholder="Email"
+            value={user.email}
             onChange={handleChange}
           />
 
           <input
-            type='password'
-            className='block border border-grey-light w-full p-3 rounded mb-4'
-            name='password'
-            placeholder='Password'
-            value={password}
+            type="password"
+            className="block border border-grey-light w-full p-3 rounded mb-4"
+            name="password"
+            placeholder="Password"
+            value={user.password}
             onChange={handleChange}
           />
 
           <button
             onClick={handleRegister}
-            type='submit'
-            className='w-full text-center py-3 rounded bg-green text-black hover:bg-green-dark focus:outline-none my-1'
+            type="submit"
+            className="w-full text-center py-3 rounded bg-green text-black hover:bg-green-dark focus:outline-none my-1"
           >
             Create Account
           </button>
         </div>
 
-        <div className='text-grey-dark mt-6'>
+        <div className="text-grey-dark mt-6">
           Already have an account?
           <a
-            className='no-underline border-b border-blue text-blue'
-            href='../login/'
+            className="no-underline border-b border-blue text-blue"
+            href="../login/"
           >
             Log in
           </a>
--- src/utils/authUtils.js
+++ src/utils/authUtils.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 const { REACT_APP_DEV_API_URL, REACT_APP_PROD_API_URL } = process.env;
 
@@ -12,18 +12,44 @@ const register = async (userData) => {
   } = await axios.post(`${REACT_APP_DEV_API_URL}/users/register`, userData);
 
   if (status !== 201) {
-    return alert('Something went wrong');
+    return alert("Something went wrong");
+  }
+  return { headers, username };
+};
+
+const login = async (userData) => {
+  const {
+    status,
+    headers,
+    data: {
+      user: { username },
+    },
+  } = await axios.post(`${REACT_APP_DEV_API_URL}/auth/login`, userData);
+
+  if (status !== 200) {
+    return alert("Something went wrong");
   }
   return { headers, username };
 };
 
 const saveToLocalStorage = (token) => {
-  localStorage.setItem('token', token);
+  localStorage.setItem("token", token);
 };
 
 const getFromLocalStorage = () => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   return token || null;
 };
 
-export { register, getFromLocalStorage, saveToLocalStorage };
+const removeFromLocalStorage = () => {
+  console.log("here");
+  localStorage.removeItem("token");
+};
+
+export {
+  register,
+  login,
+  getFromLocalStorage,
+  saveToLocalStorage,
+  removeFromLocalStorage,
+};
